a7d17ab0da816665cb03b7d47a8f793d60a72cf0
ArtemLuckoyanow <arteom.luckoyanow@yandex.ru>
2020-12-20 22:43:52 +0500

    Скопировал класс для переноса в файл

---
+++ Robot Manipulator/Robot Manipulator/JSON/LinkSerialized.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace Robot_Manipulator.JSON
+{
+    class LinkSerialized
+    {
+		public Point BeginPosition { get; set; }
+		public Point EndPosition { get; set; }
+		public float Weight { get; set; }
+		public ManipulatorElement.elementTypes ElementType { get; set; }
+
+		public LinkSerialized()
+		{
+
+		}
+
+		public LinkSerialized(Link linkToSerialize)
+		{
+			BeginPosition = linkToSerialize.BeginPosition;
+			EndPosition = linkToSerialize.EndPosition;
+			Weight = linkToSerialize.Weight;
+			ElementType = linkToSerialize.ElementType;
+		}
+	}
+}
